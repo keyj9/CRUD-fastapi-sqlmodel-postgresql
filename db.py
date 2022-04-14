683e6c44487d564affc9059feb3555122993fd4e
keyj9 <kyrillos.joe@gmail.com>
2022-04-14 14:27:12 +0300

    update db

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from sqlmodel import create_engine
 
-conn_str = "postgresql://postgres:password@localhost:5432/api"
+conn_str = "postgresql://postgres:password@localhost:5432/sqlmodeldb"
 
 engine = create_engine(conn_str, echo=True)
+
